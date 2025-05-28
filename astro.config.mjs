437c329afc09888421bf9ad4a5b65962f44ba635
leog <leog91@gmail.com>
2025-05-28 06:03:01 +0100

    tailwindcss bump to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  integrations: [tailwind()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
